1767fbf6645e3eac61def7feb7289fa8f1f2f352
gismo23 <128148026+gismo23@users.noreply.github.com>
2024-11-04 21:14:23 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MessestandViewer from './MessestandViewer'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MessestandViewer />
+  </React.StrictMode>
+)
