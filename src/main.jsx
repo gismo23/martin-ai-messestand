cadd89bda08f2c52fb251c3a7b3cffc3f1f97b32
gismo23 <128148026+gismo23@users.noreply.github.com>
2024-11-04 22:09:07 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import MessestandViewer from './MessestandViewer'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
