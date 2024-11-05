493b1483059ebd745d1703ee9f38837b99c8bc24
gismo23 <128148026+gismo23@users.noreply.github.com>
2024-11-05 08:00:11 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/martin-ai-messestand/',
-  resolve: {
-    alias: {
-      'three': 'three'
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          three: ['three']
-        }
-      }
-    }
-  }
 })
